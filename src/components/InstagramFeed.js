d4f6152880579a5780e3a6585b264f8277ec84f5
Rulesstack <65716510+Rulesstack@users.noreply.github.com>
2020-05-25 21:49:26 +0530

    insta token

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -8,7 +8,7 @@ import './InstagramFeed.css'
 
 export default class InstagramFeed extends Component {
   static defaultProps = {
-    accessToken: '1353697840.1677ed0.5a1cbfbc18f84915aa0d9a0bd02bff5a',
+    accessToken: '36136264101.452bff1.837f374ad5684fdaa7c13b117e54dde5',
     count: 20
   }
 
