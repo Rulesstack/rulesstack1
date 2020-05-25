7ed7c9b6a3da47c6e4758e0024951d66c08f5c3a
Rulesstack <65716510+Rulesstack@users.noreply.github.com>
2020-05-25 21:58:23 +0530

    insta username

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/rulesstack/">@rulesstack</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
