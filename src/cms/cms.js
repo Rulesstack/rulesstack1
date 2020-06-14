d738f77dfb4d8e18aad06e1804d02666b48f3268
Rulesstack <65716510+Rulesstack@users.noreply.github.com>
2020-06-14 16:24:33 -0500

    added NoVideoTempTemplate

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,6 +8,7 @@ import { ContactPageTemplate } from '../templates/ContactPage'
 import { DefaultPageTemplate } from '../templates/DefaultPage'
 import { BlogIndexTemplate } from '../templates/BlogIndex'
 import { SinglePostTemplate } from '../templates/SinglePost'
+import { NoVideoTempTemplate } from '../templates/NoVideoTemp'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 
 CMS.registerMediaLibrary(uploadcare)
